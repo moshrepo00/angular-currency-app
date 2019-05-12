95bfeadc2538022c8e7cdc68e5f2a398ec37a72c
mo <moshpit2241@gmail.com>
2019-05-13 00:14:11 +0800

    add loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MainComponent} from './main/main.component';
 import {HttpClientModule} from '@angular/common/http';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        MainComponent
+        MainComponent,
+        LoaderComponent
     ],
     imports: [
         BrowserModule,
