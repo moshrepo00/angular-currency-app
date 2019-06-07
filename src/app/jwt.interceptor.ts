c0d85dca2b97e9323167bd8bfe16a190f6dbd714
mo <moshpit2241@gmail.com>
2019-06-07 20:28:59 +0800

    jwt interceptor - account for third party apis

--- src/app/jwt.interceptor.ts
+++ src/app/jwt.interceptor.ts
@@ -2,25 +2,28 @@ import {Injectable} from '@angular/core';
 import {HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {AuthService} from './auth.service';
+import {DataProviderService} from './data-provider.service';
 
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(public auth: AuthService) {
+    constructor(public auth: AuthService, private dataProv: DataProviderService) {
     }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
-        const token = this.auth.getToken();
-        if (token) {
-            request = request.clone({
-                setHeaders: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`
-                }
-            });
+        console.log('DEBUGGING REQUEST', request);
+        if (request.url.includes(this.dataProv.endpoint)) {
+            const token = this.auth.getToken();
+            if (token) {
+                request = request.clone({
+                    setHeaders: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+            }
         }
-
         return next.handle(request);
     }
 }
